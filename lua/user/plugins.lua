2c2882f3f11f4572c94f297862521977942b03d5
rajesh <mrmadhan1327@gmail.com>
2023-11-11 19:36:28 +0530

    backup

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -7,6 +7,31 @@ lvim.plugins = {
     "nvim-lua/plenary.nvim",
   },
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+      -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Treesitter Search"
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc =
+        "Toggle Flash Search"
+      },
+    },
+  },
+  {
     "nvim-neorg/neorg",
     ft = "norg",   -- lazy-load on filetype
     config = true, -- run require("neorg").setup()
